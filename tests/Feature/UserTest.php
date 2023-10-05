46211171c213ddf118c760ef3da5c7626ffe1403
Praditya Aldi Syahputra <pradityaaldi.s@gmail.com>
2023-10-05 15:40:27 +0700

    Logout user API

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -218,4 +218,27 @@ class UserTest extends TestCase
             ]
         ]);
     }
+
+    // user logout
+    public function testLogoutSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->delete(uri: '/api/users/logout', headers: [
+            'Authorization' => 'test'
+        ])->assertStatus(200)
+            ->assertJson(
+                ["data" => true]
+            );
+
+        $user = User::where('username', 'test')->first();
+        self::assertNull($user->token);
+    }
+    public function testLogoutFailed()
+    {
+        $this->seed([UserSeeder::class]);
+        $this->delete(uri: '/api/users/logout', headers: [
+            'Authorization' => 'salah'
+        ])->assertStatus(401)->assertJson(["errors" => ["message" => ["unauthorized"]]]);
+    }
 }
